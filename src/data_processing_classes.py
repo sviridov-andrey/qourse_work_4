e0aab6d907b179aaf9c5397ed318db997337325c
sviridov-andrey <chosenfew@yandex.ru>
2023-05-18 23:21:57 +0300

    Добавлен __init__ в Vacansy

--- src/data_processing_classes.py
+++ src/data_processing_classes.py
@@ -10,6 +10,7 @@ class FileHandling(AbstractJson):
         self.__filename = f'{vacansy.title()}.json'
         self.all_vacansies = all_vacansies
         self.create_file()
+        print("Инициализация")
 
     def create_file(self):
         """Запись в файл списка вакансий"""
@@ -21,15 +22,35 @@ class FileHandling(AbstractJson):
     def load_file(self):
         """Загрузка из файла списка вакансий"""
         pass
-    pass
 
 
 class Vacansy:
-    def __init__(self):
-        pass
+    __slots__ = ("title", "employer", "url", "area", "experience", "employment", "salary",
+                 "salary_from", "salary_to", "currency", "portal")
+
+    def __init__(self, title, employer, url, area, experience, employment, salary,
+                 salary_from, salary_to, currency, portal):
+        self.title = title
+        self.employer = employer
+        self.url = url
+        self.area = area
+        self.experience = experience
+        self.employment = employment
+        self.salary = salary
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.currency = currency
+        self.portal = portal
+
+    def __gt__(self, other):
+        if not other.salary_from:
+            return True
+        elif not self.salary_from:
+            return False
+        return self.salary_from >= other.salary_from
 
 
-a = SJGetVacansies('менеджер')
-v = a.validate_vacansies()
-for i in v:
-    print(i)
+# a = SJGetVacansies('менеджер')
+# v = a.validate_vacansies()
+# for i in v:
+#     print(i)
